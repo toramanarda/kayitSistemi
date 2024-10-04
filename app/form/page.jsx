5198cbdfdd807a99961fcfed8a3ec1d8ea4e568c
Arda Toraman <toramanarda245@gmail.com>
2024-10-04 21:43:08 +0300

    page.jsx de form yapısı oluşturuldu. useState tanımladım. handleChange değişkeni tanımladım.

--- app/form/page.jsx
+++ app/form/page.jsx
@@ -1,7 +1,27 @@
 export default function StudentRegistration() {
+
+  const [formData, setFormData] = useState({});
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+  
   return (
     <>
       <h1>Öğrenci Kayıt Sistemi</h1>
+
+      <form onSubmit={handleSubmit}>
+        <p>
+          <label>
+            İsim: <input type="text" name="name" required onChange={handleChange}></input>
+          </label>
+        </p>
+      </form>
     </>
   )
 }
